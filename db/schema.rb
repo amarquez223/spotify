2f4009568f47fddbdea49b971b8f1c76a7ea6448
anderson <anderson@debian.anderson.com>
2017-12-15 16:14:23 -0500

    Corrección de relaciones entre modelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214230448) do
+ActiveRecord::Schema.define(version: 20171215143600) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20171214230448) do
     t.string "timestamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "artist_id"
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -34,6 +36,8 @@ ActiveRecord::Schema.define(version: 20171214230448) do
     t.string "preview_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "album_id"
+    t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
 end
